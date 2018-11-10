07b91ff86e49ba08ba5f537a64206e7a4e33d82c
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-11-10 06:24:36 -0600

    Initial commit.

---
+++ jokes/src/main/java/com/example/android/jokes/Jokes.java
@@ -0,0 +1,9 @@
+package com.example.android.jokes;
+
+public class Jokes
+{
+  public static String getJoke()
+  {
+    return "Why did the chicken cross the road? To get to the other side.";
+  }
+}
